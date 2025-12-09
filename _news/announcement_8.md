f55a0aa486ea57167d5449d5d9c96446391b5353
Boskovii <156978755+Boskovii@users.noreply.github.com>
2025-12-09 18:09:11 -0500

    Remove dark mode toggle and force dark mode always; add NSF GRFP announcement

---
+++ _news/announcement_8.md
@@ -0,0 +1,9 @@
+---
+layout: post
+date: 2025-11-13 00:00:00-0500
+inline: true
+related_posts: false
+---
+
+Submitted my NSF GRFP application, including my proposed [Graduate Research Plan](/assets/pdf/NSF%20GRFP%20Research%20Plan.pdf).
+
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -278,21 +278,6 @@ let determineComputedTheme = () => {
 };
 
 let initTheme = () => {
-  let themeSetting = determineThemeSetting();
-
-  setThemeSetting(themeSetting);
-
-  // Add event listener to the theme toggle button.
-  document.addEventListener("DOMContentLoaded", function () {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function () {
-      toggleThemeSetting();
-    });
-  });
-
-  // Add event listener to the system theme preference change.
-  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
-    applyTheme();
-  });
+  // Force dark mode always
+  setThemeSetting("dark");
 };
